ed011bcce7b35ab7f0e24e13a8b6f12657bc3193
laurenngo <lauren.nicole.go@gmail.com>
2015-06-10 10:16:19 -0700

    redid slick effect by hand and fixed layout of contact

--- js/script.js
+++ js/script.js
@@ -42,32 +42,56 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   $('#rex-thumb').on('click', function(){
     // alert('clicked rex!');
+    //text
     $('#rex-details').removeClass('hidden').addClass('show');
     $('#fstvl-details').removeClass('show').addClass('hidden');
     $('#foodli-details').removeClass('show').addClass('hidden');
     $('#wlg-details').removeClass('show').addClass('hidden');
+    //monitor photo
+    $('#rex-monitor-img').removeClass('hidden').addClass('show');
+    $('#fstvl-monitor-img').removeClass('show').addClass('hidden');
+    $('#foodli-monitor-img').removeClass('show').addClass('hidden');
+    $('#wlg-monitor-img').removeClass('show').addClass('hidden');
   });
 
    $('#fstvl-thumb').on('click', function(){
+    //text
     $('#fstvl-details').removeClass('hidden').addClass('show');
     $('#rex-details').removeClass('show').addClass('hidden');
     $('#foodli-details').removeClass('show').addClass('hidden');
     $('#wlg-details').removeClass('show').addClass('hidden');
+     //monitor photo
+    $('#fstvl-monitor-img').removeClass('hidden').addClass('show');
+    $('#rex-monitor-img').removeClass('show').addClass('hidden');
+    $('#foodli-monitor-img').removeClass('show').addClass('hidden');
+    $('#wlg-monitor-img').removeClass('show').addClass('hidden');
 
   });
 
    $('#foodli-thumb').on('click', function(){
+    //text
     $('#foodli-details').removeClass('hidden').addClass('show');
     $('#fstvl-details').removeClass('show').addClass('hidden');
     $('#rex-details').removeClass('show').addClass('hidden');
     $('#wlg-details').removeClass('show').addClass('hidden');
+     //monitor photo
+    $('#foodli-monitor-img').removeClass('hidden').addClass('show');
+    $('#rex-monitor-img').removeClass('show').addClass('hidden');
+    $('#fstvl-monitor-img').removeClass('show').addClass('hidden');
+    $('#wlg-monitor-img').removeClass('show').addClass('hidden');
   });
 
    $('#wlg-thumb').on('click', function(){
+    //text
     $('#wlg-details').removeClass('hidden').addClass('show');
     $('#fstvl-details').removeClass('show').addClass('hidden');
     $('#foodli-details').removeClass('show').addClass('hidden');
     $('#rex-details').removeClass('show').addClass('hidden');
+     //monitor photo
+    $('#wlg-monitor-img').removeClass('hidden').addClass('show');
+    $('#rex-monitor-img').removeClass('show').addClass('hidden');
+    $('#fstvl-monitor-img').removeClass('show').addClass('hidden');
+    $('#foodli-monitor-img').removeClass('show').addClass('hidden');
   });
 
 
